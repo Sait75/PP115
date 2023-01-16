76f8d1d84a86eaf2e41238453b260be437f341e1
Sait75 <saa4981@gmail.com>
2023-01-16 10:41:59 +0300

    correcting main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,32 +1,27 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
-import jm.task.core.jdbc.util.Util;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.sql.*;
-import java.util.Calendar;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
 
     public static void main(String[] args) {
-//        Util.getConnection();
 
-        UserDao userDao = new UserDaoJDBCImpl();
-//
-        userDao.dropUsersTable();
-        userDao.createUsersTable();
-//
-      userDao.saveUser("Name1", "LastName1", (byte) 20);
-//    userDao.saveUser("Name2", "LastName2", (byte) 25);
-//    userDao.saveUser("Name3", "LastName3", (byte) 31);
-//    userDao.saveUser("Name4", "LastName4", (byte) 38);
-//
-//    userDao.removeUserById(2);
-//    userDao.getAllUsers();
-//        userDao.cleanUsersTable();
+        UserService userService = new UserServiceImpl();
+
+        userService.dropUsersTable();
+        userService.dropUsersTable();
+        userService.createUsersTable();
+        userService.createUsersTable();
+
+        userService.saveUser("Name1", "LastName1", (byte) 20);
+        userService.saveUser("Name2", "LastName2", (byte) 25);
+        userService.saveUser("Name3", "LastName3", (byte) 31);
+        userService.saveUser("Name4", "LastName4", (byte) 38);
+
+        userService.removeUserById(2);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
 
 
     }
